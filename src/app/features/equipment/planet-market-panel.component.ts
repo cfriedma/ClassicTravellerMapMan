c25a078332887b6ed853190ed5f40b0c2769a6c1
cfriedma <twainsaidit@gmail.com>
2026-09-17 22:25:43 -0700

    Update psionics legality display: Remove conditional display of Psionic Institute mention in the planet market panel for clarity. This change simplifies the message shown to users regarding psionics on the world.

--- src/app/features/equipment/planet-market-panel.component.ts
+++ src/app/features/equipment/planet-market-panel.component.ts
@@ -81,7 +81,7 @@ type MarketTabId = 'trade_goods' | string;
       <div class="tab-body" *ngIf="activeTab !== 'trade_goods'">
         <p class="psi-banner" *ngIf="psionicsEnabled && activeTab === 'drugs'">
           <ng-container *ngIf="world.isPsionicsPermitted(); else psiIllegal">
-            Psionics are permitted on this world<span *ngIf="world.hasPsionicInstitute"> (Psionic Institute)</span>.
+            Psionics are permitted on this world.
           </ng-container>
           <ng-template #psiIllegal>
             Psionics are illegal on this world<span *ngIf="punishmentLabel"> ({{ punishmentLabel }})</span>.
